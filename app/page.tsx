f63205366d2faca3b94f48063f92faab0bc3665a
kgtoledoc <kgtoledoc19@gmail.com>
2025-06-06 05:23:59 -0500

    add new button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <p>Hello World</p>;
+  return <Button variant={"destructive"}>Click me</Button>;
 }
